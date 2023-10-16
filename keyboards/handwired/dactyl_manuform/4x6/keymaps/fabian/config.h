aeb6e00fb2189ee2b2b59120b4b6e1591ebb6c50
Fabian Topfstedt <topfstedt@schneevonmorgen.com>
2023-10-16 09:23:21 +0200

    fabian dactyl manuform

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/fabian/config.h
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/fabian/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #undef PRODUCT
-#define PRODUCT Dactyl Manuform 4x6 (fabian)
+#define PRODUCT "Dactyl Manuform 4x6 (fabian)"
 
 // #define MATRIX_ROW_PINS { F7, B1, B3, B2, B6 }
 // #define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
