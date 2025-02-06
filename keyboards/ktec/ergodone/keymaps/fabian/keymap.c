9e64df64d06dc2ef023d010c3b94041b658889f8
Fabian Topfstedt <topfstedt@schneevonmorgen.com>
2025-02-06 14:45:12 +0100

    ergodone fabian: having different base layers

--- keyboards/ktec/ergodone/keymaps/fabian/keymap.c
+++ keyboards/ktec/ergodone/keymaps/fabian/keymap.c
@@ -1,13 +1,21 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 
-#define BASE 0 // default layer
-#define LOWR 1 // symbols
-#define RAIS 2 // media keys
+#define BAS1 0 // default layer
+#define BAS2 1
+#define LOWR 2
+#define RAIS 3
 
 enum custom_keycodes {
   VRSN = SAFE_RANGE,
 };
+
+enum base_layer {
+    _BASE_1,
+    _BASE_2
+};
+
+uint8_t current_base_layer = _BASE_1;
 
 // Custom key behavior
 #define CTL_ESC CTL_T(KC_ESC)  // Tap for Escape, hold for Control
@@ -19,12 +27,54 @@ enum custom_keycodes {
 #define SFT_ENT SFT_T(KC_ENT)  // Tap for Enter, hold for Shift
 #define SFT_SPC SFT_T(KC_SPC)  // Tap for Space, hold for Shift
 #define UMLAUT  RALT(KC_U)     // Combine Alt and U
+#define BAS_TOG MO(1)
 //
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | MehGrv |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
+ * | Grv    |   1  |   2  |   3  |   4  |   5  |BasTog|           |      |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |  Bspc  |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | CapsLck|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   | LCtl |  Grv | LGui | LAlt | Lower|                                       |Raise | RAlt | RGui |Umlaut| RCtl |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,---------------.
+ *                                        | LGui | Del  |       | Alt  |Ctrl/Esc|
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      |      | Home |       | PgUp |        |      |
+ *                                 | Spc  |Backsp|------|       |------|  Tab   |SftEnt|
+ *                                 |      |ace   | End  |       | PgDn |        |      |
+ *                                 `--------------------'       `----------------------'
+ */
+[BAS1] = LAYOUT_ergodox(
+        // left hand
+        KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   BAS_TOG,
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_TRNS,
+        KC_CAPS,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_TRNS,
+        KC_LCTL,        KC_GRV,       KC_LGUI,KC_LALT,MO(LOWR),
+                                              KC_LGUI,        KC_DEL,
+                                                              KC_HOME,
+                                              KC_SPC, KC_BSPC,KC_END,
+        // right hand
+             KC_TRNS,     KC_6,   KC_7,    KC_8,   KC_9,   KC_0,      KC_MINS,
+             KC_TRNS,     KC_Y,   KC_U,    KC_I,   KC_O,   KC_P,      KC_BSPC,
+                          KC_H,   KC_J,    KC_K,   KC_L,   KC_SCLN,   KC_QUOT,
+             KC_TRNS,     KC_N,   KC_M,    KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,
+                                  MO(RAIS),KC_RALT,KC_RGUI,UMLAUT,    KC_RCTL,
+             KC_LALT,        CTL_ESC,
+             KC_PGUP,
+             KC_PGDN,KC_TAB, SFT_ENT
+    ),
+/*
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * | MehGrv |   1  |   2  |   3  |   4  |   5  |BasTog|           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | HprTab |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |  Bspc  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -42,9 +92,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |ace   | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
-[BASE] = LAYOUT_ergodox(
+[BAS2] = LAYOUT_ergodox(
         // left hand
-        MEH_GRV,        KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_TRNS,
+        MEH_GRV,        KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   BAS_TOG,
         HPR_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_TRNS,
         CTL_ESC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         SFT_BSP,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_TRNS,
@@ -146,6 +196,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
+void set_base_layer(uint8_t base_layer) {
+    eeconfig_update_user(base_layer); // Save to EEPROM
+
+    layer_off(BAS1);
+    layer_off(BAS2);
+
+    switch (base_layer) {
+        case _BASE_1:
+            layer_on(BAS1);
+            break;
+        case _BASE_2:
+            layer_on(BAS2);
+            break;
+    }
+};
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case VRSN:
@@ -153,6 +219,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
       }
       return false;
+    case BAS_TOG:
+        if (record->event.pressed) {
+            current_base_layer++;
+            if (current_base_layer > _BASE_2) {
+                current_base_layer = _BASE_1;  // cycle back to 1
+            }
+            set_base_layer(current_base_layer);
+        }
+        return false;
   }
   return true;
 }
@@ -161,6 +236,11 @@ bool led_update_user(led_t led_state) {
     // stub any kb level behaviour
     return false;
 }
+
+void keyboard_post_init_user(void) {
+    current_base_layer = eeconfig_read_user();
+    set_base_layer(current_base_layer);
+};
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
@@ -181,5 +261,4 @@ void matrix_scan_user(void) {
             // none
             break;
     }
-
 };
