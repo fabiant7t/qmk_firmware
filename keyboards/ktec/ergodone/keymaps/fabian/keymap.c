c051e1769e4eb8be9a9f8f21dfaceefbd0746477
Fabian Topfstedt <topfstedt@schneevonmorgen.com>
2025-02-06 09:07:58 +0100

    ergodone: fabian layout with hyper/space

--- keyboards/ktec/ergodone/keymaps/fabian/keymap.c
+++ keyboards/ktec/ergodone/keymaps/fabian/keymap.c
@@ -13,6 +13,7 @@ enum custom_keycodes {
 #define CTL_ESC CTL_T(KC_ESC)  // Tap for Escape, hold for Control
 #define RGUISPC RGUI_T(KC_SPC) // Tap for Space, hold for right GUI
 #define HPR_TAB HYPR_T(KC_TAB) // Tap for Tab, hold for Hyper (Super+Ctrl+Alt+Shift)
+#define HPR_SPC HYPR_T(KC_SPC) // Tap for Space, hold for Hyper (Super+Ctrl+Alt+Shift)
 #define MEH_GRV MEH_T(KC_GRV)  // Tap for Backtick, hold for Meh (Ctrl+Alt+Shift)
 #define SFT_BSP SFT_T(KC_BSPC) // Tap for Backspace, hold for Shift
 #define SFT_ENT SFT_T(KC_ENT)  // Tap for Enter, hold for Shift
@@ -37,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        | LGui | Del  |       | Alt  |Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
- *                                 |GuiSpc|Backsp|------|       |------|  Tab   |SftEnt|
+ *                                 |HprSpc|Backsp|------|       |------|  Tab   |SftEnt|
  *                                 |      |ace   | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -50,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,        MEH_GRV,      KC_LGUI,KC_LALT,MO(LOWR),
                                               KC_LGUI,        KC_DEL,
                                                               KC_HOME,
-                                              RGUISPC,KC_BSPC,KC_END,
+                                              HPR_SPC,KC_BSPC,KC_END,
         // right hand
              KC_TRNS,     KC_6,   KC_7,    KC_8,   KC_9,   KC_0,      KC_MINS,
              KC_TRNS,     KC_Y,   KC_U,    KC_I,   KC_O,   KC_P,      KC_BSPC,
